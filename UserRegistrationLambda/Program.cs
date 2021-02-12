e2efae5105dcd1e9c5dfda2a98b12be640ef235c
diliprathod32 <diliprathod32@gmail.com>
2021-02-12 22:10:25 +0530

    [Dilip] WelcomeToUserRegistrationLambda

---
+++ UserRegistrationLambda/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to user registration lambda Expression");
+        }
+    }
+}
